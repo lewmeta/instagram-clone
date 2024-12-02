92850f1a05619fedc199c53bb022f650d33e66d1
lewmeta <dev.lewismeta@gmail.com>
2024-12-02 19:57:03 +0300

    feat(video-component): create Instagram-like video component with Alpine.js
    - Built a reusable video component using Alpine.js to mimic Instagram's behavior.
    - Added functionality for play/pause toggle with  and .
    - Integrated dynamic UI updates such as showing play/pause icons based on state.
    - Ensured responsiveness and clean styling for seamless integration.
    - Included event handlers for interaction and keyboard accessibility.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import intersect from '@alpinejs/intersect'
+import { livewire_hot_reload } from 'virtual:livewire-hot-reload'
 
 window.Alpine = Alpine;
 
+Alpine.plugin(intersect)
 Alpine.start();
 
-import { livewire_hot_reload } from 'virtual:livewire-hot-reload'
 livewire_hot_reload();
